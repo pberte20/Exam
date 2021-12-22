cca40881a83872ca2632bf7bacbbd8392df14552
pberte20 <71381584+pberte20@users.noreply.github.com>
2021-12-22 01:56:10 +0100

    Added stregsystem class and some functions

--- StregSystem.cs
+++ StregSystem.cs
@@ -1,12 +1,64 @@
 namespace Exam
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     public class StregSystem
     {
-        public static void Main()
+    
+        private List<Transaction> _transactions;
+        private List<Product> _products;
+        private List<User> _users;
+        
+        public BuyProductTransaction BuyProduct(Product product, User user, decimal amount)
         {
-            var system = new Stregsystem();
-            system.Start();
+            BuyProductTransaction transaction = new BuyProductTransaction(product, user, amount);
+            transactionExecute(transaction);
+            return transaction;
+        }
+
+        public InsertCashTransaction InsertCash(decimal amount, User user)
+        {
+            InsertCashTransaction transaction = new InsertCashTransaction(amount, user);
+            transactionExecute(transaction);
+            return transaction;
+        }
+
+        public Product GetProductById(int id)
+        {
+            foreach (Product product in _products)
+            {
+                if (product.Id == id)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<User> GetUsers(Func<User, bool> predicate)
+        {
+            return _users.Where(predicate);
+        }
+
+        public User GetUserByUserName(string userName)
+        {
+            User user = _users.Where(u => u.UserName == userName).FirstOrDefault();
+            if (user != null)
+            {
+                return user;
+            }
+            else
+            {
+                throw new UserNotFoundException();
+            }
+        }
+
+
+        private void transactionExecute(Transaction transaction)
+        {
+            transaction.Execute();
+            _transactions.Add(transaction);
         }
     }
 }
